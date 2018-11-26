93f8a1b5474a0ba2cb720ab97ce7bbab714b3f11
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-11-26 23:04:08 +0200

    initial commit

---
+++ P03.EnglishNameOfTheLastDigit/Startup.cs
@@ -0,0 +1,57 @@
+﻿namespace programming_fundamentals
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            long number = long.Parse(Console.ReadLine());
+
+            string lastDigitName = GetLastDigitName(number);
+            Console.WriteLine(lastDigitName);
+        }
+
+        static string GetLastDigitName(long number)
+        {
+            number = Math.Abs(number);
+            long digit = number % 10;
+
+            string lastDigitName = "";
+            switch (digit)
+            {
+                case 0:
+                    lastDigitName = "zero";
+                    break;
+                case 1:
+                    lastDigitName = "one";
+                    break;
+                case 2:
+                    lastDigitName = "two";
+                    break;
+                case 3:
+                    lastDigitName = "three";
+                    break;
+                case 4:
+                    lastDigitName = "four";
+                    break;
+                case 5:
+                    lastDigitName = "five";
+                    break;
+                case 6:
+                    lastDigitName = "six";
+                    break;
+                case 7:
+                    lastDigitName = "seven";
+                    break;
+                case 8:
+                    lastDigitName = "eight";
+                    break;
+                case 9:
+                    lastDigitName = "nine";
+                    break;
+            }
+
+            return lastDigitName;
+        }
+    }
+}
